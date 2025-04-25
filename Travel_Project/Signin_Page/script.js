1225a10ac3d86ab252661ce70b1c64bded542232
A-PRASATHARUMUGAM <prasatharumugam040@gmail.com>
2025-04-25 23:23:33 +0530

    Off implement

--- Travel_Project/Signin_Page/script.js
+++ Travel_Project/Signin_Page/script.js
@@ -1,1 +1,9 @@
-console.log('Hello world ')
+const mainContainer=document.querySelector(".main-container");
+const heroSection=document.querySelector(".hero-section");
+
+
+mainContainer.addEventListener("click",()=>{
+
+  heroSection.classList.add('active');
+
+})
